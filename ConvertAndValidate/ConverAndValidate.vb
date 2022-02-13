3ae80e87e2aa4fc287f9697270eb4c6f7f53c9f3
Smitfall <captainfalconrsmith@gmail.com>
2022-02-13 15:53:15 -0700

    Add project files.

---
+++ ConvertAndValidate/ConverAndValidate.vb
@@ -0,0 +1,47 @@
+﻿'Fallon Smith
+'RCET 0265
+'Spring 2022
+'CovertAndValidate
+'
+
+Option Explicit On
+Option Strict On
+
+Module ConverAndValidate
+
+    Sub Main()
+        'for testing the function add this to your Sub Main()
+        Dim userResponse As String
+        Dim aValidNumber As Integer
+
+        Do
+
+            Console.WriteLine($"Enter a number:")
+
+            userResponse = Console.ReadLine()
+
+            If ConversionValid(userResponse, CBool(aValidNumber)) = True Then
+                Console.WriteLine($"{userResponse} converted successfully to {userResponse}!")
+            Else
+                Console.WriteLine($"Oops, {userResponse} does not seem to be a number")
+            End If
+
+        Loop
+    End Sub
+
+    Function ConversionValid(convertThisString As String, toThisInteger As Boolean) As Boolean
+
+        Dim status As Boolean
+
+        Try
+            Convert.ToInt32(convertThisString)
+            status = True
+
+        Catch ex As Exception
+
+        End Try
+
+        Return status
+
+    End Function
+End Module
